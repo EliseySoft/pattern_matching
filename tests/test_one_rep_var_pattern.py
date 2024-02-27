0fdf47fd8f0d30b39a6696c1c80210cfd40783c4
eliseymyagkikh <el2002@inbox.ru>
2024-02-27 19:02:26 +0300

    поправил тест

--- tests/test_one_rep_var_pattern.py
+++ tests/test_one_rep_var_pattern.py
@@ -7,7 +7,7 @@ from utils import check_pattern
 @pytest.mark.parametrize('s, pattern, expect_success', [
     ('astkxciayvcgcaxnfkiwmownrcogjiwmownrcomownrcocjyagsfss', 'a_x2_i_a_y_v_x3_x1_x4_x1_x1_x5', True),
     ('pcpyzwltlpgddiswrctuoot', 'p_x2_x1_x1_x3_x4_x5', True),
-    ('aabb', 'x1_x1_x2_x2', True),
+    ('aabb', 'x1_x1_x2_x3', True),
     ('aababcaabacdaabade', 'a_x1_a_x2_a_x1_a_x3_a_x1_a_x4', True),
     ('aaaaaaaaaaaa', 'a_x1_a_x2_a_x1_a_x3_a_x1_a_x4', True),
     ('abbcaabcabbcbbcad', 'a_b_x1_a_a_x1_x2_b_c_x3_x1_a_x4', True)
